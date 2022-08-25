6ff88cfb517687ae212f7deb9a85ab56a9a964a7
JuliDeveloper <rja-97@yandex.ru>
2022-08-25 22:46:30 +0300

    Added 'add' button and function for this

--- ОrderСontrol/ViewControllers/CustomersTableViewController.swift
+++ ОrderСontrol/ViewControllers/CustomersTableViewController.swift
@@ -20,7 +20,7 @@ class CustomersTableViewController: UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
         
-        setUpButton()
+        setUpButtons()
         fetchData()
         tableView.reloadData()
     }
@@ -38,24 +38,32 @@ extension CustomersTableViewController {
         
         var content = cell.defaultContentConfiguration()
         content.text = customer.name
+        content.secondaryText = customer.info
         cell.contentConfiguration = content
         cell.selectionStyle = .none
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let customer = customers[indexPath.row]
+        
         let detailsVC = CustomerDetails()
         navigationController?.pushViewController(detailsVC, animated: true)
+        detailsVC.customer = customer
     }
 }
 
 //MARK: - Private func
 extension CustomersTableViewController {
-    private func setUpButton() {
+    private func setUpButtons() {
         let backButton = UIBarButtonItem()
         backButton.title = "Back"
         backButton.tintColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
         navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(presentCustomerDetails))
+        addButton.tintColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        navigationItem.rightBarButtonItem = addButton
     }
     
     private func fetchData() {
@@ -67,5 +75,11 @@ extension CustomersTableViewController {
                 print(error)
             }
         }
+    }
+    
+    @objc private func presentCustomerDetails(sender: AnyObject?) {
+        let detailsVC = CustomerDetails()
+        navigationController?.pushViewController(detailsVC, animated: true)
+        detailsVC.customer = sender as? Customer
     }
 }
